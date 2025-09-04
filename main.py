969f21bd6a8858206fcaf3719b9682ee6de9fc3a
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-04 09:06:36 +0100

    code commit by ENGR. IPAYE

--- main.py
+++ main.py
@@ -34,5 +34,30 @@ def add_toys(toy: Toy):
 def get_toys():
     return {"toys": toys}
 
-# READ (Get a single toy by ID)
 
+# READ (Get a single toy by ID)
+@app.get("/toys/{toys_id}")
+def get_toy(toy_id: int):
+    for toy in toys:
+        if toy["id"] == toy_id:
+            return toy
+        raise HTTPException(status_code=404, detail="Toy not found")
+
+
+# UPDATE (edit toy by ID)
+@app.put("/toys/{toy_id}")
+def update_toy(toy_id: int, updated_toy: Toy):
+    for index, toy in enumerate(toys):
+        if toy["id"] == toy_id:
+            toys[index] = updated_toy.dict()
+            return HTTPException(status_code=404, detail="Toy not found")
+
+
+# Delete (Remove toy by id)
+@app.delete("/toys/{toy_id}")
+def delete_toy(toy_id: int):
+    for index, toy in enumerate(toys):
+        if toy["id"] == toy_id:
+            deleted = toys.pop(index)
+            return {"message": "Toy deleted successfully", "toys": deleted}
+        raise HTTPException(status_code=404, detail="Toy not found")
