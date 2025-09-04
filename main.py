f635021dfe7f9c45d4b033708408e1441d4d308d
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-04 10:37:45 +0100

    code commit by ENGR. IPAYE

--- main.py
+++ main.py
@@ -1,64 +1,73 @@
 # Fast api
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models, schemas
+from database import engine, SessionLocal
 
-# initialize fast api app
-app = FastAPI(title="Toy store Api", version="1.0")
+# Create tables
+models.Base.metadata.create_all(bind=engine)
 
-# In-memory database
-toys = []
+app = FastAPI(title="Toy store API", version="2.0")
 
-
-# pydantic model(defines structures of data)
-class Toy(BaseModel):
-    id: int
-    name: str
-    price: float
-    in_stock: bool
+# Dependency: get DB Session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 # ------- CRUD ENDPOINT ------#
-# CREATE (Add new Toy)
-@app.post("/toys/")
-def add_toys(toy: Toy):
-    # check if toy id exist
-    for t in toys:
-        if t["id"] == toy.id:
-            raise HTTPException(status_code=400, detail="Toy with this ID already exist")
-
-    toys.append(toy.dict())
-    return {"message": "toys added successfully", "toy": toy}
+# CREATE (POST) (Add new Toy)
+@app.post("/toys/", response_model=schemas.Toy)
+def create_toy(toy: schemas.ToyCreate, db: Session = Depends(get_db)):
+    db_toy = models.Toy(**toy.dict())
+    db.add(db_toy)
+    db.commit()
+    db.refresh(db_toy)
+    return db_toy
 
 
 # READ (Get all toys)
-@app.get("/toys/")
-def get_toys():
-    return {"toys": toys}
+@app.get("/toys/", response_model=list[schemas.Toy])
+def read_toys(db: Session = Depends(get_db)):
+    return db.query(models).Toy.all()
 
 
 # READ (Get a single toy by ID)
-@app.get("/toys/{toy_id}")
-def get_toy(toy_id: int):
-    for toy in toys:
-        if toy["id"] == toy_id:
-            return toy
+@app.get("/toys/{toy_id}", response_model=schemas.Toy)
+def read_toy(toy_id: int, db: Session = Depends(get_db)):
+    toy = db.query(models.Toy).filter(models.Toy.id == toy_id).first()
+    if not toy:
         raise HTTPException(status_code=404, detail="Toy not found")
+    return toy
 
 
 # UPDATE (edit toy by ID)
-@app.put("/toys/{toy_id}")
-def update_toy(toy_id: int, updated_toy: Toy):
-    for index, toy in enumerate(toys):
-        if toy["id"] == toy_id:
-            toys[index] = updated_toy.dict()
-            return HTTPException(status_code=404, detail="Toy not found")
+@app.put("/toys/{toy_id}", response_model=schemas.Toy)
+def update_toy(toy_id: int, updated_toy: schemas.ToyCreate, db: Session = Depends(get_db)):
+    toy = db.query(models.Toy).filter(models.Toy.id == toy_id).first()
+    if not toy:
+        raise HTTPException(status_code=404, detail="Toy not found")
+
+    toy.name = updated_toy.name
+    toy.price = updated_toy.price
+    toy.in_stock = updated_toy.in_stock
+
+    db.commit()
+    db.refresh(toy)
+    return toy
+
 
 
 # Delete (Remove toy by id)
 @app.delete("/toys/{toy_id}")
-def delete_toy(toy_id: int):
-    for index, toy in enumerate(toys):
-        if toy["id"] == toy_id:
-            deleted = toys.pop(index)
-            return {"message": "Toy deleted successfully", "toys": deleted}
+def delete_toy(toy_id: int, db: Session = Depends(get_db)):
+    toy = db.query(models.Toy).filter(models.Toy.id == toy_id).first()
+    if not toy:
         raise HTTPException(status_code=404, detail="Toy not found")
+
+    db.delete(toy)
+    db.commit()
+    return {"message": "Toy deleted successfully"}
